771e24b66214392f61ce7f16225114422e720e7b
Anna <108558769+annajesus@users.noreply.github.com>
2022-12-20 23:16:44 -0500

    save work

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,3 @@
-// 1. Use the D3 library to read in samples.json from the URL 
-    
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 const dataSample = 'Data/samples.json'
 
@@ -23,10 +21,7 @@ const dataSample = 'Data/samples.json'
 
     })
 
-// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-// Use sample_values as the values for the bar chart.
-// Use otu_ids as the labels for the bar chart.
-// Use otu_labels as the hovertext for the chart.
+
 
 // create function for plotting all of the data
 function getPlot(id){
@@ -34,10 +29,6 @@ function getPlot(id){
 // Use the D3 library to get data from samples.json 
 d3.json(url).then((data)=> {
     console.log(data)
-
-    var wfreq = data.metadata.map(d => d.wfreq)
-
-    console.log(`Washing Freq: ${wfreq}`)
 
     // filter the sample values by each of the ids (in "samples:")
     var samples = data.samples.filter(s => s.id === id)[0];
@@ -90,14 +81,6 @@ d3.json(url).then((data)=> {
     // bar plot 
     Plotly.newPlot('bar', bar_data, bar_layout)  
 
-
-
-// 3.Create a bubble chart that displays each sample.
-// Use otu_ids for the x values.
-// Use sample_values for the y values.
-// Use sample_values for the marker size.
-// Use otu_ids for the marker colors.
-// Use otu_labels for the text values.
 
     // Create the bubble chart
     var trace_bubble = {
@@ -161,7 +144,7 @@ function getData(id) {
 // create the optionChanged function
 function optionChanged(id) {
     getPlot(id);
-    getInfo(id);
+    getData(id);
 
 
 // create the function for the initial data rendering
@@ -180,7 +163,7 @@ function init() {
 
         // call the functions 
         getPlot(data.names[0]);
-        getInfo(data.names[0]);
+        getData(data.names[0]);
     });
 }
 
